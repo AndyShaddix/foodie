23da05f1db521e5d9ed53635ed462d9720a479af
AndyShaddix <andresf.gaxiolae@gmail.com>
2023-07-04 21:18:10 -0700

    Change Colors

--- lib/src/colors/colors.dart
+++ lib/src/colors/colors.dart
@@ -5,3 +5,4 @@ final gris = Color.fromRGBO(184, 187, 198, 1.0);
 final fondoGris = Color.fromRGBO(189, 189, 179, 1);
 final verdeUTN = Color.fromARGB(255, 5, 87, 15);
 final bgEntradas = Color.fromRGBO(142, 142, 147, 1.2);
+final negro = Color.fromRGBO(0, 0, 0, 1);
